3fe050f4507dfee2157594fcd6cf3f03f0daafd3
Krishan Shamod <krishanshamod@gmail.com>
2022-04-10 22:19:59 +0530

    Added Location model

---
+++ app/src/main/java/com/krishanshamod/simple_android_application_3/model/Location.kt
@@ -0,0 +1,20 @@
+package com.krishanshamod.simple_android_application_3.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "location")
+data class Location(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+
+    @ColumnInfo(name = "locationName")
+    val locationName: String,
+
+    @ColumnInfo(name = "latitude")
+    val latitude: Double,
+
+    @ColumnInfo(name = "longitude")
+    val longitude: Double,
+)
